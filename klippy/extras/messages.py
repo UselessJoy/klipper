ec6c626f406a5a2acec54f0e279d41391b601393
UselessJoy <mrqwer2014@mail.ru>
2024-04-01 16:37:58 +0300

    Update messages.py
    add new messages

--- klippy/extras/messages.py
+++ klippy/extras/messages.py
@@ -3,21 +3,32 @@ import locales
 class Messages:
     def __init__(self, config):
         self.printer = config.get_printer()
+        self.gcode = self.printer.lookup_object('gcode')
         self.is_open = False
-        self.messages = {   'warning': 
+        self.timer = None
+        self.webhooks_messages = {   'warning': 
                             {   
                                 'on_wait_temperature': _("Heating with the wait parameter, gcodes will be processed after warming up"),
-                                'on_open_door_or_hood': _("Printing is paused. Printing will be continued after closing doors and hood"), ## locale
+                                'on_open_door_or_hood': _("Printing is paused. Printing will be continued after closing doors and hood"),
                                 'wait_for_pause': _("Printer already go to pause"),
-                                'wait_for_resume': _("Printer already wait go to resume")
+                                'wait_for_resume': _("Printer already wait go to resume"),
+                                'has_next_screw': _("Cannot set new calibrating screw: already wait move to next screw %s")
                             },
                             'success': 
-                            {'saved_default_color': _("New default color was successfully saved!")
+                            {'saved_default_color': _("New default color was successfully saved!"),
+                             'successfull_save_bed_mesh': _("Bed mesh was successfull saved!"),
                              },
                             'suggestion': 
-                            {},
+                            {
+                                
+                            },
                             'error':
-                            {},
+                            {
+                                "calibration_not_perfomed": _("Cannot set new calibrating screw: calibration is not performed"),
+                                "screw_not_defined": _("Cannot set new calibrating screw: screw is not defined"),
+                                "screw_is_base_screw": _("Cannot set new calibrating screw: screw %s is base screw"),
+                                "screw_not_in_screws": _("Cannot set new calibrating screw: screw %s not in defined screws")
+                            },
                         }
         self.message_type = ""
         self.current_message = ""
@@ -30,15 +41,19 @@ class Messages:
     def _handle_open_message(self, web_request):
         message_type = web_request.get_str('message_type')
         message = web_request.get_str('message')
-        self.send_message(message_type, message)
+        self.send_message(message_type, message, True)
         
-    def send_message(self, message_type, message):
+    def send_message(self, message_type, message, from_webhooks=False):
         self.message_type = message_type
         self.last_eventtime = self.reactor.monotonic()
-        self.current_message = self.messages[self.message_type][message]
+        if from_webhooks:
+            self.current_message = self.webhooks_messages[self.message_type][message]
+        else:
+            self.current_message = message
         self.reset_open_timer()
         self.timer = self.reactor.register_timer(self.open_timer, self.reactor.NOW)
-        self.is_open = True    
+        self.is_open = True
+        self.gcode.respond_msg(self.current_message, f"({self.message_type})", True)
         
     def open_timer(self, eventtime):
         if abs(eventtime - self.last_eventtime) > 10:

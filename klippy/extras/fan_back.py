8227b87e37d601c81caef852421540fe2d6b1ee3
UselessJoy <mrqwer2014@mail.ru>
2024-07-25 11:31:55 +0300

    Update fan_back.py

--- klippy/extras/fan_back.py
+++ klippy/extras/fan_back.py
@@ -6,7 +6,7 @@ class PrinterFanBack:
         self.printer = config.get_printer()
         self.fan = fan.Fan(config, default_shutdown_speed=0.)
         self.fan_name = config.get_name().split()[-1]
-        self.last_host_temp = self.last_mcu_temp = 0
+        self.last_host_temp = self.last_mcu_temp = self.last_speed = 0
         gcode = self.printer.lookup_object("gcode")
         gcode.register_mux_command("SET_FAN_SPEED", "FAN",
                                    self.fan_name,
@@ -30,10 +30,11 @@ class PrinterFanBack:
       self.set_speed(temp)
 
     def set_speed(self, temp):
-      if temp >= 55 and self.fan.last_fan_value < 100:
-        self.fan.set_speed_from_command(80 + (temp - 40))
+      if temp >= 55:
+        self.last_speed = 80 + (temp - 40)
       else:
-        self.fan.set_speed_from_command(80)
+        self.last_speed = 80
+      self.fan.set_speed_from_command(self.last_speed)
         
     def _handle_ready(self):
         self.fan.set_speed_from_command(80)
@@ -43,6 +44,10 @@ class PrinterFanBack:
     def cmd_SET_FAN_SPEED(self, gcmd):
         speed = gcmd.get_float('SPEED', 0.)
         self.fan.set_speed_from_command(speed)
+    
+    def get_status(self, eventtime):
+       return {'setting_speed': self.last_speed,
+               'last_fan_value': self.fan.last_fan_value}
 
 def load_config_prefix(config):
     return PrinterFanBack(config)

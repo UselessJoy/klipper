2ab20145b6c199998ecf503b890d763a72af5978
UselessJoy <mrqwer2014@mail.ru>
2024-07-25 11:25:00 +0300

    Update fan_back.py

--- klippy/extras/fan_back.py
+++ klippy/extras/fan_back.py
@@ -6,21 +6,30 @@ class PrinterFanBack:
         self.printer = config.get_printer()
         self.fan = fan.Fan(config, default_shutdown_speed=0.)
         self.fan_name = config.get_name().split()[-1]
-        self.max_temp = 0
+        self.last_host_temp = self.last_mcu_temp = 0
         gcode = self.printer.lookup_object("gcode")
         gcode.register_mux_command("SET_FAN_SPEED", "FAN",
                                    self.fan_name,
                                    self.cmd_SET_FAN_SPEED,
                                    desc=self.cmd_SET_FAN_SPEED_help)
-        self.printer.register_event_handler("klippy:ready", self._handle_ready)
-        self.printer.register_event_handler("temperature_host:sample_temperature", self._on_temp)
-        self.printer.register_event_handler("temperature_mcu:sample_temperature", self._on_temp)
+        self.register_event_handler("klippy:ready", self._handle_ready)
+        self.register_event_handler("temperature_host:sample_temperature", self._on_host_temp)
+        self.register_event_handler("temperature_mcu:sample_temperature", self._on_mcu_temp)
     
 
-    def _on_temp(self, temp):
-      if temp <= self.max_temp:
-          return
-      self.max_temp = temp
+    def _on_host_temp(self, temp):
+      self.last_host_temp = temp
+      if temp <= self.last_mcu_temp:
+         return
+      self.set_speed(temp)
+
+    def _on_mcu_temp(self, temp):
+      self.last_mcu_temp = temp
+      if temp <= self.last_host_temp:
+         return
+      self.set_speed(temp)
+
+    def set_speed(self, temp):
       if temp >= 55 and self.fan.last_fan_value < 100:
         self.fan.set_speed_from_command(80 + (temp - 40))
       else:

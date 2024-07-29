7e4d49f226be8cbe3bf3006d4eacde8160eb3ee8
UselessJoy <mrqwer2014@mail.ru>
2024-07-29 10:01:44 +0300

    Update probe.py

--- klippy/extras/probe.py
+++ klippy/extras/probe.py
@@ -108,8 +108,7 @@ class PrinterProbe:
         self.printer.register_event_handler("klippy:ready",
                                             self._on_ready)
         self.reactor = self.printer.get_reactor()
-        self.magnet_checker_timer = self.reactor.register_timer(
-                    self.magnet_check, self.reactor.NOW)
+        
     
     def magnet_check(self, eventtime):
         toolhead = self.printer.lookup_object('toolhead')
@@ -124,6 +123,8 @@ class PrinterProbe:
         res = self.mcu_probe.query_endstop(print_time)
         self.last_state = res
         self.is_using_magnet_probe = not bool(res)
+        self.magnet_checker_timer = self.reactor.register_timer(
+                    self.magnet_check, self.reactor.NOW)
 
     def run_gcode_get_magnet(self):
         gcode = self.printer.lookup_object('gcode')

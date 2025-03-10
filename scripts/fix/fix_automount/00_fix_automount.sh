bfa250bfc892ac03e8bfad7b7c5169d76757b311
UselessJoy <mrqwer2014@mail.ru>
2025-03-10 16:40:51 +0300

    update scripts

--- scripts/fix/fix_automount/00_fix_automount.sh
+++ scripts/fix/fix_automount/00_fix_automount.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 if [ -d "$HOME/udev-media-automount" ]; then
-  rm -rf udev-media-automount
+  rm -rf $HOME/udev-media-automount
 fi
 cd $HOME
 git clone https://github.com/UselessJoy/udev-media-automount
-cd udev-media-automount
+cd $HOME/udev-media-automount
 echo orangepi | sudo --stdin make install
 echo orangepi | sudo --stdin udevadm control --reload-rules
 echo orangepi | sudo --stdin udevadm trigger
--- scripts/fix/fix_automount/01_cp_gcodes.sh
+++ scripts/fix/fix_automount/01_cp_gcodes.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 if [ -d "$HOME/printer_data/mmcblk0p1/" ]; then
-  cp -r /home/orangepi/printer_data/gcodes/* /home/orangepi/printer_data/mmcblk0p1/gcodes/
+  cp -r $HOME/printer_data/gcodes/* $HOME/printer_data/mmcblk0p1/gcodes/
 fi
 exit 0

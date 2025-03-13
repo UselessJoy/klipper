7686239b11216959711199bc7a88267e269a6663
UselessJoy <mrqwer2014@mail.ru>
2025-03-13 14:18:55 +0300

    Обновление скрипта

--- scripts/fix/fix_automount/01_cp_gcodes.sh
+++ scripts/fix/fix_automount/01_cp_gcodes.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 if [ -d "$HOME/printer_data/mmcblk0p1/" ]; then
   cp -r $HOME/printer_data/gcodes/* $HOME/printer_data/mmcblk0p1/gcodes/
+  rm -rf $HOME/printer_data/gcodes/*
 fi
 exit 0

9443774e07609cc66f5dc4efa6fc5c35eb1fed71
Farinov <mrqwer2014@mail.ru>
2025-10-24 11:20:31 +0300

    Add script for assistant

---
+++ scripts/fix/fix_assistant/00_install_assistant.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+detect_password() {
+  if command -v apt &> /dev/null; then
+    echo "orangepi"
+  elif command -v dnf &> /dev/null; then
+    echo "user"
+  else
+    echo "unknown"
+  fi
+}
+pwd=$(detect_password)
+
+detect_package_manager() {
+  if command -v apt &> /dev/null; then
+    echo "apt"
+  elif command -v dnf &> /dev/null; then
+    echo "dnf"
+  else
+    echo "unknown"
+  fi
+}
+PKG_MANAGER=$(detect_package_manager)
+
+cd $HOME
+
+if [[ "$PKG_MANAGER" == "apt" ]]; then
+	wget -O cassistant.deb https://мойассистент.рф/скачать/Download/1329
+	echo "$pwd" | sudo --stdin dpkg -i cassistant.deb
+else
+	wget -O cassistant.rpm https://мойассистент.рф/скачать/Download/1328
+	echo "$pwd" | sudo --stdin rpm -i cassistant.rpm
+fi
+echo "$pwd" | sudo --stdin stop assistant.service
+echo "$pwd" | sudo --stdin disable assistant.service
+exit 0

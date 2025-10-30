ac791e8b635da875534eb6c506b17ae9b64bf742
UselessJoy <mrqwer2014@mail.ru>
2025-10-30 14:03:35 +0300

    Update 00_install_assistant.sh

--- scripts/fix/fix_assistant/00_install_assistant.sh
+++ scripts/fix/fix_assistant/00_install_assistant.sh
@@ -1,31 +1,38 @@
-#!/bin/sh
+#!/bin/bash
+
 detect_password() {
-  if command -v apt &> /dev/null; then
-    echo "orangepi"
-  elif command -v dnf &> /dev/null; then
-    echo "user"
-  else
-    echo "unknown"
-  fi
+    if command -v apt &> /dev/null; then
+        echo "orangepi"
+    elif command -v dnf &> /dev/null; then
+        echo "user"
+    else
+        echo "unknown"
+    fi
 }
-pwd=$(detect_password)
 
 detect_package_manager() {
-  if command -v apt &> /dev/null; then
-    echo "apt"
-  elif command -v dnf &> /dev/null; then
-    echo "dnf"
-  else
-    echo "unknown"
-  fi
+    if command -v apt &> /dev/null; then
+        echo "apt"
+    elif command -v dnf &> /dev/null; then
+        echo "dnf"
+    else
+        echo "unknown"
+    fi
 }
+
+pwd=$(detect_password)
 PKG_MANAGER=$(detect_package_manager)
 
 if [[ "$PKG_MANAGER" == "apt" ]]; then
-	echo "$pwd" | sudo --stdin dpkg -i cassistant_6.5-1_arm64.deb
+    echo "$pwd" | sudo -S dpkg -i cassistant_6.5-1_arm64.deb
+elif [[ "$PKG_MANAGER" == "dnf" ]]; then
+    echo "$pwd" | sudo -S rpm -i cassistant-6.5-1.aarch64.rpm
 else
-	echo "$pwd" | sudo --stdin rpm -i cassistant-6.5-1.aarch64.rpm
+    echo "Unsupported package manager: $PKG_MANAGER"
+    exit 1
 fi
-echo "$pwd" | sudo --stdin systemctl stop assistant.service
-echo "$pwd" | sudo --stdin systemctl disable assistant.service
+
+echo "$pwd" | sudo -S systemctl stop assistant.service
+echo "$pwd" | sudo -S systemctl disable assistant.service
+
 exit 0
